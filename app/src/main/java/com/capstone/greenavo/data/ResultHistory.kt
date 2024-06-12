79519a9859e1e17a2282d3dba283bb2d8aee0e15
Gery <yokogery02@gmail.com>
2024-06-12 14:58:29 +0800

    Fix Result Detection & History

--- app/src/main/java/com/capstone/greenavo/data/ResultHistory.kt
+++ app/src/main/java/com/capstone/greenavo/data/ResultHistory.kt
@@ -3,6 +3,6 @@ package com.capstone.greenavo.data
 data class ResultHistory (
     var id: String = "", // Add an ID field to store the Firestore document ID
     val image_url: String = "",
-    val kematangan: String = "",
+    val label: String = "",
     val score: String = ""
 )
