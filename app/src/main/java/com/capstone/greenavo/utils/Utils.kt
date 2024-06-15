94fd306246927742aaafcd208c5c8efeb175c04c
Gery <yokogery02@gmail.com>
2024-06-16 01:55:58 +0800

    Update Gradle

--- app/src/main/java/com/capstone/greenavo/utils/Utils.kt
+++ app/src/main/java/com/capstone/greenavo/utils/Utils.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
@@ -11,6 +12,7 @@ import android.provider.MediaStore
 import androidx.core.content.FileProvider
 import androidx.exifinterface.media.ExifInterface
 import com.bumptech.glide.load.resource.bitmap.TransformationUtils
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils.rotateImage
 import com.capstone.greenavo.BuildConfig
 import java.io.ByteArrayOutputStream
 import java.io.File
@@ -90,10 +92,18 @@ fun Bitmap.getRotatedBitmap(file: File): Bitmap? {
         ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED
     )
     return when (orientation) {
-        ExifInterface.ORIENTATION_ROTATE_90 -> TransformationUtils.rotateImage(this, 90)
-        ExifInterface.ORIENTATION_ROTATE_180 -> TransformationUtils.rotateImage(this, 180)
-        ExifInterface.ORIENTATION_ROTATE_270 -> TransformationUtils.rotateImage(this, 270)
+        ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(this, 90F)
+        ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(this, 180F)
+        ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(this, 270F)
         ExifInterface.ORIENTATION_NORMAL -> this
         else -> this
     }
+}
+
+fun rotateImage(source: Bitmap, angle: Float): Bitmap {
+    val matrix = Matrix()
+    matrix.postRotate(angle)
+    return Bitmap.createBitmap(
+        source, 0, 0, source.width, source.height, matrix, true
+    )
 }
